36f2af28cc2619e29f2cbe8ec32eee663cb25c58
NotRllyRn <75143746+NotRllyRn@users.noreply.github.com>
2022-08-02 11:58:02 -0700

    oh this might be a painfully long wait time

---
+++ index.js
@@ -0,0 +1,7 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+const ws = require("ws");
+const crypto = require("crypto-js");
+
